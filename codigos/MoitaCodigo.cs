65e741361982b10b0dadc92affce569c88f815d7
Coffe99p <126524268+Coffe99p@users.noreply.github.com>
2023-04-10 18:30:27 -0300

    Update MoitaCodigo.cs

--- codigos/MoitaCodigo.cs
+++ codigos/MoitaCodigo.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 public class MoitaCodigo : MonoBehaviour
 {
-   
-    public GeradorMoita geradorMoita;
+    public GeradorMoita geradorMoita; // Referência ao objeto do gerador de moitas
 
-
-    
-
-    
     void Update()
     {
-        transform.Translate(Vector2.left * geradorMoita.CurrentSpeed * Time.deltaTime); 
+        transform.Translate(Vector2.left * geradorMoita.CurrentSpeed * Time.deltaTime); // Move a moita para a esquerda com base na velocidade atual do gerador
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("NextLine"))
+        if (collision.gameObject.CompareTag("NextLine")) // Verifica se a colisão ocorreu com um objeto com a tag "NextLine"
         {
-            geradorMoita.GerarProximaCmEspaco();
+            geradorMoita.GerarProximaCmEspaco(); // Chama o método do gerador para gerar a próxima moita com um espaço de tempo aleatório
         }
-         if (collision.gameObject.CompareTag("Finish"))
+        if (collision.gameObject.CompareTag("Finish")) // Verifica se a colisão ocorreu com um objeto com a tag "Finish"
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // Destroi a moita atual
         }
     }
 }
